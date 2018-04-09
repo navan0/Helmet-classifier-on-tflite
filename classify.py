fe0aae8148c40306db843b4a9b2ca6ce93fcf84b
navaneeth <nktclt@gmail.com>
2018-04-09 23:00:39 +0530

    Added README

---
+++ classify.py
@@ -0,0 +1,26 @@
+import tensorflow as tf, sys
+
+image_path = sys.argv[1]
+
+image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+
+label_lines = [line.rstrip() for line
+		  in tf.gfile.GFile("/home/navaneeth/helmetclassifier/retrained_labels.txt")]
+
+with tf.gfile.FastGFile("/home/navaneeth/helmetclassifier/retrained_graph.pb",'rb') as f:
+	graph_def = tf.GraphDef()
+	graph_def.ParseFromString(f.read())
+	_ = tf.import_graph_def(graph_def,name='')
+
+with tf.Session() as sess:
+	softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+	
+	predictions = sess.run(softmax_tensor, \
+			{'DecodeJpeg/contents:0': image_data})
+
+	top_k = predictions[0].argsort()[-1:][::-1]
+	
+	for node_id in top_k:
+	    human_string = label_lines[node_id]
+	    score = predictions[0][node_id]
+print('%s (score = %.5f)' % (human_string, score))
